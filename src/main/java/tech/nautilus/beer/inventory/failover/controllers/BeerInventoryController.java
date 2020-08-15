3c1774beee3f6ee055267fdc5a6c8396937a5ed7
Oleksandr Sytnik <Oleksandr.Sytnik@rabobank.nl>
2020-08-15 14:05:30 +0200

    Added a basic implementation of the beer-inventory-failover service.

--- src/main/java/tech/nautilus/beer/inventory/failover/controllers/BeerInventoryController.java
+++ src/main/java/tech/nautilus/beer/inventory/failover/controllers/BeerInventoryController.java
@@ -5,13 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import tech.nautilus.beer.inventory.service.repositories.BeerInventoryRepository;
-import tech.nautilus.beer.inventory.service.web.mappers.BeerInventoryMapper;
 import tech.nautilus.brewery.model.BeerInventoryDto;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  * Created by jt on 2019-05-31.
@@ -21,10 +19,18 @@ import java.util.stream.Collectors;
 @RestController
 public class BeerInventoryController {
 
+    public static final int QUANTITY_ON_HAND = 999;
+
     @GetMapping("api/v1/beer/{beerId}/inventory")
     List<BeerInventoryDto> listBeersById(@PathVariable UUID beerId){
         log.debug("Failover Inventory Response for beerId:" + beerId);
 
-        return List.of(BeerInventoryDto.builder().build());
+        return List.of(BeerInventoryDto.builder()
+                .id(UUID.randomUUID())
+                .createdDate(OffsetDateTime.now())
+                .lastModifiedDate(OffsetDateTime.now())
+                .beerId(beerId)
+                .quantityOnHand(QUANTITY_ON_HAND)
+                .build());
     }
 }
